822da676aaae45554ecac1c7784a71a857972572
JoksiP <58534820+JoksiP@users.noreply.github.com>
2022-06-19 21:06:28 +0200

    fixed concurrent implementation in python

--- python_implementation/concurrent_impl.py
+++ python_implementation/concurrent_impl.py
@@ -1,57 +1,53 @@
+from math import sqrt
+
 import numpy as np
 import multiprocessing as mp
 import time
 import write_utils
-import copy
 
-# Each thread / process
-def conc_process(matrix1, matrix2, k_proc, M, L):
-    result_matrix = np.zeros((M, L))
+
+def thread_mult(matrix1, matrix2, N, p_step_i, p_step_j, p_sqrt):
+    result_matrix = np.zeros((N, N))
     # perform shift by process number
-    #print(matrix1)
-    for i in range(M):
-        # shift left
-        matrix1[i] = np.roll(matrix1[i], -k_proc)
-    for i in range(L):
-        # shift up
-        matrix2[:, i] = np.roll(matrix2[:, i], -k_proc)
-    # matrix multiplication
-    for i in range(M):
-        for j in range(L):
-            result_matrix[i][j] += matrix1[i][j] * matrix2[i][j]
-    write_utils.write_conc_to_csv(k_proc, matrix1, matrix2, result_matrix)
+    # print(matrix1)
+    for k in range(p_sqrt*p_sqrt):
+        # matrix multiplication
+        for i in range(p_step_i, p_step_i + p_sqrt):
+            for j in range(p_step_j, p_step_j + p_sqrt):
+                result_matrix[i][j] += matrix1[i][j] * matrix2[i][j]
+        # perform shift by 1
+        for i in range(N):
+            # shift left
+            matrix1[i] = np.roll(matrix1[i], -1)
+        for i in range(N):
+            # shift up
+            matrix2[:, i] = np.roll(matrix2[:, i], -1)
+    write_utils.write_conc_to_csv(p_step_i * N + p_step_j, matrix1, matrix2, result_matrix)
     return result_matrix
 
-"""# Thread callback
-def collect_result(result):
-    global results_matrix
-    results_matrix += result"""
 
-
-# Main body
-def begin(matrix1, matrix2, P, M, L):
-    results_matrix = np.zeros((M, L))
+def begin(matrix1, matrix2, P, N):
     write_utils.init_conc()
     start = time.time()
+    result = np.zeros((N,N))
     # perform initial shift
-    for i in range(M):
+    for i in range(N):
         # shift left
         matrix1[i] = np.roll(matrix1[i], -i)
-    for i in range(L):
-        # shift up
         matrix2[:, i] = np.roll(matrix2[:, i], -i)
 
+    p_sqrt = int(sqrt(P))
+    n_p_sqrt = int(N/p_sqrt)
     # Init multiprocessing.Pool()
     pool = mp.Pool(mp.cpu_count())
     results = []
-    for k in range(P):
-        #m1 = copy.deepcopy(matrix1)
-        #m2 = copy.deepcopy(matrix2)
-        results.append(pool.apply_async(conc_process, args=(matrix1, matrix2, k, M, L)))
+    for i in range(n_p_sqrt):
+        for j in range(n_p_sqrt):
+            results.append(pool.apply_async(thread_mult, args=(matrix1, matrix2, N, i*p_sqrt, j*p_sqrt, p_sqrt)))
     for i in range(P):
-        results_matrix = np.add(results_matrix, results[i].get())
+        result = np.add(result, results[i].get())
     pool.terminate()
     pool.close()
     pool.join()
-    print(results_matrix)
-    return time.time() - start
+    end = time.time()
+    return end - start
--- python_implementation/main.py
+++ python_implementation/main.py
@@ -6,21 +6,21 @@ import copy
 
 def main():
     # m x n
-    matrix1 = np.random.randint(1, 10, size=(200, 200))
+    matrix1 = np.random.randint(1, 10, size=(256, 256))
     # n x l
-    matrix2 = np.random.randint(1, 10, size=(200, 200))
+    matrix2 = np.random.randint(1, 10, size=(256, 256))
     # m x l
     M = len(matrix1)
     N = len(matrix2)
     L = len(matrix2[0])
     result_matrix = np.zeros((M, L))
     # number of processes
-    P = N
-    print(np.matmul(matrix1, matrix2))
-    elapsed = sequential_impl.begin(copy.deepcopy(matrix1), copy.deepcopy(matrix2), result_matrix, P, M, L)
+    P = 16
+    #print(np.matmul(matrix1, matrix2))
+    elapsed = sequential_impl.begin(copy.deepcopy(matrix1), copy.deepcopy(matrix2), result_matrix, N, M, L)
     print(elapsed)
     result_matrix = np.zeros((M, L))
-    elapsed = concurrent_impl.begin(matrix1, matrix2, P, M, L)
+    elapsed = concurrent_impl.begin(matrix1, matrix2, P, N)
     print(elapsed)
 
 
