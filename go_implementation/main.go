e15d66455cfe36cdef91a5b2fb2a90dd55bf173c
JoksiP <58534820+JoksiP@users.noreply.github.com>
2022-07-10 08:27:07 +0200

    command line args added

--- go_implementation/main.go
+++ go_implementation/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
+	"strconv"
 )
 
 func main() {
-	N := 256
-	P := 256
+	N, _ := strconv.Atoi(os.Args[1])
+	P, _ := strconv.Atoi(os.Args[2])
+
+	p_sqrt := int(math.Sqrt(float64(P)))
+	if p_sqrt*p_sqrt != P {
+		fmt.Println("P must be a perfect square number.")
+		os.Exit(0)
+	}
+	n_p_sqrt := float64(N) / float64(p_sqrt)
+	if n_p_sqrt != float64(int(n_p_sqrt)) {
+		fmt.Println("N must be dividable by square root of P")
+		os.Exit(0)
+	}
+
 	matrix1 := createMatrix(N, 0.0, 10.0)
 	matrix2 := createMatrix(N, 0.0, 10.0)
 	//printMatrix(matrix1)
